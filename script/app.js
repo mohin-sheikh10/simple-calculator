b2e796639d2c6d535922e71337f99af22bce5c40
mohin-sheikh10 <mohinshei014@gmail.com>
2024-08-24 15:19:10 +0600

    Fix Input Validation

--- script/app.js
+++ script/app.js
@@ -5,6 +5,14 @@ addNumberBtn.addEventListener("click", function () {
   const num1Value = parseFloat(inputNum1.value);
   const inputNum2 = document.getElementById("num2");
   const num2Value = parseFloat(inputNum2.value);
+  if (isNaN(num1Value)) {
+    alert("Please Fill Up Your Number Field");
+    return;
+  }
+  if (isNaN(num2Value)) {
+    alert("Please Fill Up Your Number Field");
+    return;
+  }
   const sum = num1Value + num2Value;
   result.innerText = `Result: ${sum}`;
   inputNum1.value = "";
